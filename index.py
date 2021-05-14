d6cd9c23aad0323bb6a53650430baf50c9ad920e
Vittor Guilherme <vittytuuguilherme@gmail.com>
2021-05-13 23:44:14 -0300

    add

--- index.py
+++ index.py
@@ -15,9 +15,9 @@ def image_to_array(name_image_02):
     arr = np.array(img)
     return arr
 
+
 # Sum
 def sum_and_ponder_array(arr_01, arr_02, lines_columns):
-
     arr = arr_01
 
     for col in range(0, lines_columns):
@@ -31,6 +31,7 @@ def sum_and_ponder_array(arr_01, arr_02, lines_columns):
         # print("---------- end of line -----------------")
     return arr
 
+
 # Subtraction
 def sub_and_ponder_array(arr_01, arr_02, lines_columns):
     arr = arr_01
@@ -38,7 +39,7 @@ def sub_and_ponder_array(arr_01, arr_02, lines_columns):
         for lin in range(0, lines_columns):
             num_01 = arr_01[lin][col]
             num_02 = arr_02[lin][col]
-            if (int(num_01) - int(num_02) < 0):
+            if int(num_01) - int(num_02) < 0:
                 pound = 0
             else:
                 pound = (int(num_01) - int(num_02)) / 2
@@ -47,6 +48,18 @@ def sub_and_ponder_array(arr_01, arr_02, lines_columns):
         # print("---------- end of line -----------------")
     return arr
 
+
+# Neighborhood
+
+def NN_interpolation(img, height, width):
+    screen_h, screen_w = img.shape
+    new_arr = np.zeros((height, width, 3), dtype=np.uint8)
+    for i in range(height - 1):
+        for j in range(width - 1):
+            scrx = round(i * (screen_h / height))
+            scry = round(j * (screen_w / width))
+            new_arr[i, j] = img[scrx, scry]
+    return new_arr
 
 # Variables
 first_image_path_name_image_01 = './first_small_img_test_10_10.jpg'
@@ -78,4 +91,14 @@ arr2 = sub_and_ponder_array(image_to_array(first_image_name_image_02), image_to_
 Image.fromarray(arr).save('./new-converted-image-01.jpg')
 Image.fromarray(arr2).save('./new-converted-image-02.jpg')
 
+
+im_path = './first_small_img_test_10_10.jpg'
+image = np.array(Image.open(im_path))
+
+image1 = NN_interpolation(image, image.shape[0] * 2, image.shape[1] * 2)
+image1 = Image.fromarray(image1.astype('uint8'))
+image1.save('./first_small_img_test_10_10_out.jpg.jpg')
+
 print("\nEnd of processing... ")
+
+
