2e63225297c6da37855625a88a4a200d9c09b6ff
VittorGuih <vittytuuguilherme@gmail.com>
2021-05-19 21:52:34 -0300

    adiciona função de rotação

--- index.py
+++ index.py
@@ -49,6 +49,26 @@ def sub_and_ponder_array(arr_01, arr_02, lines_columns):
         # print("---------- end of line -----------------")
     return arr
 
+# rotation
+
+def image_rotation_identity(rotate_degree, lines_columns):
+
+    theta = np.radians(rotate_degree)
+    c, s = np.cos(theta), np.sin(theta)
+    a = np.array(((c, -s), (s, c)))
+    
+    identity = np.identity(lines_columns)
+    identity[0][0] = c
+    identity[0][1] = -s
+    identity[1][0] = s
+    identity[1][1] = c
+
+    return identity.astype(int)
+
+def image_rotation(arr_image, arr_identity):
+
+    return arr_identity.dot(arr_image)
+
 
 # Neighborhood
 
@@ -99,6 +119,23 @@ image = np.array(Image.open(im_path))
 image1 = NN_interpolation(image, image.shape[0] * 2, image.shape[1] * 2)
 image1 = Image.fromarray(image1.astype('uint8'))
 image1.save('./first_small_img_test_10_10_out.jpg.jpg')
+
+
+# rotation of 180 degrees
+
+identity = image_rotation_identity(180, 10)
+second_image = image_to_array('./first_small_img_test_10_10.jpg')
+second_image = second_image.astype(int)
+arr_rotate_result = image_rotation(second_image, identity)
+arr_rotate_result = arr_rotate_result
+
+print("\n------------------- Rotate Image -----------------------------\n\n")
+
+print(arr_rotate_result)
+
+# Create a new image after rotation process
+Image.fromarray(arr_rotate_result.astype('uint8')).save('./new-rotated-image-01.jpg')
+
 
 print("\nEnd of processing... ")
 
